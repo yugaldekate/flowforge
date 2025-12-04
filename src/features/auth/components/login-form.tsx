17ad757cbeb9b681021fc9e2855ad20bc7696947
Yugal D Dekate <yugaldekate72@gmail.com>
2025-12-04 13:39:55 +0530

    29: github google auth

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -42,6 +42,32 @@ export function LoginForm () {
         }
     });
 
+    const signInGithub = async () => {
+        await authClient.signIn.social({
+            provider: "github"
+        }, {
+            onSuccess: () => {
+                router.push("/");
+            },
+            onError: () => {
+                toast.error("Something went wrong");
+            },
+        });
+    }
+
+    const signInGoogle = async () => {
+        await authClient.signIn.social({
+            provider: "google"
+        }, {
+            onSuccess: () => {
+                router.push("/");
+            },
+            onError: () => {
+                toast.error("Something went wrong");
+            },
+        });
+    }
+
     const onSubmit = async(values : LoginFormValues) => {
         await authClient.signIn.email({
             email: values.email,
@@ -73,6 +99,7 @@ export function LoginForm () {
                             <div className="grid gap-6">
                                 <div className="flex flex-col gap-4">
                                     <Button
+                                        onClick={signInGithub}
                                         variant="outline"
                                         className="w-full"
                                         type="button"
@@ -87,6 +114,7 @@ export function LoginForm () {
                                         Continue with GitHub
                                     </Button>
                                     <Button
+                                        onClick={signInGoogle}
                                         variant="outline"
                                         className="w-full"
                                         type="button"
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -14,6 +14,16 @@ export const auth = betterAuth({
         enabled: true,
         autoSignIn: true,
     },
+    socialProviders: {
+        github: { 
+            clientId: process.env.GITHUB_CLIENT_ID as string, 
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
+        },
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        },
+    },
     plugins:[
         polar({
             client: polarClient, 
