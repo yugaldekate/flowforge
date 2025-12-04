c7b69afcd75c0d1bcd9a25829766a7fcf93b51fb
Yugal D Dekate <yugaldekate72@gmail.com>
2025-12-04 15:11:08 +0530

    30: deployment

--- src/lib/polar.ts
+++ src/lib/polar.ts
@@ -5,5 +5,5 @@ import { Polar } from "@polar-sh/sdk";
 // Access tokens obtained in Production are for instance not usable in the Sandbox environment.
 export const polarClient = new Polar({ 
     accessToken: process.env.POLAR_ACCESS_TOKEN, 
-    server: "sandbox", // TODO: change to 'production' when ready
+    server: "sandbox", // TODO: change to 'sandbox' for development and "production" when ready
 });
